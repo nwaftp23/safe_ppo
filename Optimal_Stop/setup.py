60f65be76ccc621acb778299f4068a1b183af540
Lucas Berry <lucasdberry@gmail.com>
2018-03-22 17:38:49 -0400

    test

--- Optimal_Stop/setup.py
+++ Optimal_Stop/setup.py
@@ -4,7 +4,7 @@ setup(name='Optimal_Stop',
       version='0.0.1',
       install_requires=['gym==0.7.4',
                         'pygame',
-                        'numpy']
+                        'numpy'],
       description='Open AI Gym environment to test risk averse algorithms',
       url='https://github.com/nwaftp23/safe_ppo/tree/master/Optimal_Stop',
       author='Lucas Berry',
